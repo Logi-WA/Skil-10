faa591c8608e1c13f1b2e3983ae6233c331e8933
Logi-WA <logi.arg@gmail.com>
2023-11-27 22:57:00 +0000

    Initial Commit - Allt

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import './App.css';
+import { allowDrop, drag, drop } from './drag.js';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <header className="header">
+          <h1>Færa Sam 🧔‍♂️</h1>
+          <p>
+            Þessi síða notar{' '}
+            <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API">
+              HTML Drag and Drop API
+            </a>{' '}
+            til þess að færa Sam milli rammanna. Prófaðu að draga hann í annan
+            ramma.
+          </p>
+        </header>
+
+        <div className="rammar">
+          <div
+            className="rammi"
+            id="rammi1"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          >
+            <img
+              src="./sam.jpg"
+              draggable="true"
+              onDragStart={drag}
+              id="drag1"
+              alt="Mynd af Sam."
+            />
+          </div>
+
+          <div
+            className="rammi"
+            id="rammi2"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi3"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi4"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi5"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi6"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi7"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi8"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi9"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi10"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi11"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+
+          <div
+            className="rammi"
+            id="rammi12"
+            onDrop={drop}
+            onDragOver={allowDrop}
+          ></div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
